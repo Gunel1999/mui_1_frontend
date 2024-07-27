c7924ac319b9da79e2a768ac2b4b6874a21aed64
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-07-27 18:43:32 +0400

    responsiveness fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,42 @@
 import React, { useContext } from 'react';
 import { LoginRegContext } from '../context/LoginReg/LoginReg';
-import { Button, Container, Paper, Skeleton, Stack } from '@mui/material';
+import { Button, Container, Paper, Skeleton, Stack, Grid } from '@mui/material';
 
 const Home = () => {
   const { setValue } = useContext(LoginRegContext);
+
   return (
     <Container maxWidth="lg">
-      <Paper sx={{ padding: 3, maxWidth: 700, minWidth: 600 }} elevation={3}>
+      <Paper
+        sx={{
+          padding: 3,
+          margin: 'auto',
+          maxWidth: { xs: '100%', sm: 700 },
+          minWidth: { xs: '90%', sm: 600 },
+        }}
+        elevation={3}
+      >
         <Stack spacing={1}>
-          {/* For variant="text", adjust the height via font-size */}
           <Skeleton variant="text" sx={{ fontSize: '3rem' }} />
-          {/* For other variants, adjust the size with `width` and `height` */}
           <Skeleton variant="circular" width={150} height={150} />
-          <Skeleton variant="rounded" maxWidth={700} height={80} />
-          <Skeleton variant="rounded" maxWidth={700} height={60} />
+          <Skeleton variant="rounded" width="100%" height={80} />
+          <Skeleton variant="rounded" width="100%" height={60} />
         </Stack>
       </Paper>
-      <Button
-        sx={{
-          mt: 3,
-          backgroundColor: '#FF8E53',
-          ':hover': { backgroundColor: '#FE6B8B' },
-        }}
-        variant="contained"
-        onClick={() => setValue('login')}
-      >
-        Log out
-      </Button>
+      <Grid container justifyContent="center">
+        <Button
+          sx={{
+            mt: 3,
+            backgroundColor: '#FF8E53',
+            ':hover': { backgroundColor: '#FE6B8B' },
+            width: { xs: '100%', sm: 'auto' },
+          }}
+          variant="contained"
+          onClick={() => setValue('login')}
+        >
+          Log out
+        </Button>
+      </Grid>
     </Container>
   );
 };
